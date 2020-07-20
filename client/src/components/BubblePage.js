a6919940ff69e8ce299e79d4041cc204c67ab5d0
Kirk Tolliver <kirktolliver28@gmail.com>
2020-07-20 11:36:23 -0500

    Fecthed Data from ColorLIst

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,7 +8,15 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+useEffect(() => {
+  axiosWithAuth()
+  .get("/api/colors")
+  .then(res => {
+    console.log("GET", res);
+    setColorList(res.data)
+  })
+  .catch(err => console.log(err))
+}, []);
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
