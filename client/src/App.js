2ce63a3459504cf916805b55355fdd8d1cb3b993
Kirk Tolliver <kirktolliver28@gmail.com>
2020-07-19 21:25:54 -0500

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
@@ -9,10 +10,14 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <Route exact path="/BubblePage" component={BubblePage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
-        */}
+        */
+        <Link to="/protected"> Protected Page</Link>}
+        <Link to="/login"> Protected Page</Link>}
+        
       </div>
     </Router>
   );
